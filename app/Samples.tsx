efad83a07bad47c7d53cde6fc0b6a7879b1168cd
EsinShadrach <esinshadrachrafe@gmail.com>
2023-11-23 23:58:05 +0100

    empty commit

---
+++ app/Samples.tsx
@@ -0,0 +1,4 @@
+"use client";
+export function Samples() {
+  return <div>apple</div>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { HeroSection } from "./HeroSection";
+import { Samples } from "./Samples";
 
 export default async function Home() {
   await new Promise((resolve) => setTimeout(resolve, 2000));
   return (
     <div className="container mx-auto p-3">
       <HeroSection />
+      <Samples />
     </div>
   );
 }
